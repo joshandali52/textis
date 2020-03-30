ca788dbeecdd74bee69e17ccee5ef1b687647fe2
michaelgau <6x24ylsGaum>
2020-03-30 20:51:35 +0200

    added admin ojbects

--- FrontEnd/webapp/textis/admin.py
+++ FrontEnd/webapp/textis/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from textis.models import WordCompare, WordTree, Syllables
+
+
+class SyllablesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text')
+    search_fields = ['text']
+
+admin.site.register(Syllables, SyllablesAdmin)
+
+
+class WordCompareAdmin(admin.ModelAdmin):
+    list_display = ('id', 'word1', 'word2')
+    search_fields = ['word1']
+
+admin.site.register(WordCompare, WordCompareAdmin)
+
+
+class WordTreeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'word')
+    search_fields = ['word']
+
+admin.site.register(WordTree, WordTreeAdmin)
